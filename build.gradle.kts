405b1f1dd9d7123a7958d2ccd2c8c648c15639c3
Vadym <vadimvrachenko@gmail.com>
2026-05-30 21:02:54 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    application
+}
+
+group = "org.example"
+
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.jetbrains.exposed:exposed-core:0.50.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.50.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.50.1")
+
+    implementation("org.xerial:sqlite-jdbc:3.46.0.0")
+
+    implementation("com.formdev:flatlaf:3.5")
+}
+
+kotlin {
+
+    jvmToolchain(17)
+}
+
+application {
+
+    mainClass.set("MainKt")
+}
